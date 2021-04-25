d6c5f5ddeef3fdc56c96393d14999201ee1dc107
quildtide <42811940+quildtide@users.noreply.github.com>
2021-04-24 20:48:59 -0400

    Add V_Canopus_V personality

--- ui/mods/com.pa.quildtide.ai-variations/new_game.js
+++ ui/mods/com.pa.quildtide.ai-variations/new_game.js
@@ -8,7 +8,6 @@ if (!VAIVLoaded) {
 
     personalities = [
       {
-        ai_path: "/pa/v-arcturus-v",
         name: "V_Arcturus_V",
         display_name: '!LOC:V_Arcturus_V',
         percent_open_bot: 1,
@@ -48,7 +47,6 @@ if (!VAIVLoaded) {
         starting_location_evaluation_radius: 400
       },
       {
-        ai_path: "/pa/v-arcturus-v",
         name: "V_Betelgeuse_V",
         display_name: '!LOC:V_Betelgeuse_V',
         percent_open_bot: 1,
@@ -87,6 +85,45 @@ if (!VAIVLoaded) {
         max_advanced_fabbers: 30,
         starting_location_evaluation_radius: 400
       },
+      {
+        name: "V_Canopus_V",
+        display_name: '!LOC:V_Canopus_V',
+        percent_open_bot: 1,
+        percent_vehicle: 0.20,
+        percent_bot: 0.10,
+        percent_air: 0.05,
+        percent_naval: 0.40,
+        percent_orbital: 0.25,
+        personality_tags:
+        [
+            "Default",
+            "PreventsWaste"
+        ],
+        metal_drain_check: 0.4,
+        energy_drain_check: 0.85,
+        metal_demand_check: 0.71,
+        energy_demand_check: 0.9,
+        micro_type: 2,
+        go_for_the_kill: true,
+        priority_scout_metal_spots: true,
+        enable_commander_danger_responses: true,
+        neural_data_mod: 1.35,
+        unable_to_expand_delay: 0,
+        per_expansion_delay: 0,
+        factory_build_delay_min: 0,
+        adv_eco_mod: 1.0,
+        adv_eco_mod_alone: 0.6,
+        fabber_to_factory_ratio_basic: 1.0,
+        fabber_to_factory_ratio_advanced: 1.0,
+        fabber_alone_on_planet_mod: 2.0,
+        basic_to_advanced_factory_ratio: 0,
+        factory_alone_on_planet_mod: 0.5,
+        min_basic_fabbers: 3,
+        max_basic_fabbers: 100,
+        min_advanced_fabbers: 0,
+        max_advanced_fabbers: 30,
+        starting_location_evaluation_radius: 400
+      }
     ]
 
     for (p of personalities) {
